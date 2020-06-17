e7c24dc6ec14373fdce1659b612079d461b448f7
Lucas Gomes <cprpc@hotmail.com>
2020-06-16 21:24:32 -0300

    Project has been structured!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GameComponent } from './pages/game/game.component';
+import { EndGameComponent } from './pages/end-game/end-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameComponent,
+    EndGameComponent
   ],
   imports: [
     BrowserModule,
